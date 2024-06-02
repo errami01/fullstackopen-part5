e458d070d5f95d1868116de86fcbdf4e46385dc2
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-06-02 21:55:02 +0100

    Add 5.7 Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isHidden, setIsHidden] = useState(true)
+  const toggleVisibility = ()=> setIsHidden(!isHidden)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return(
+  <div style={blogStyle}>
+    {blog.title} {blog.author} 
+    <button onClick={toggleVisibility}>{isHidden ? 'view':'hide'}</button>
+    {!isHidden && <div>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes} <button>like</button></p>
+      <p>{blog.user?.name} {blog.user?.username}</p>
+    </div>}
   </div>  
-)
+)}
 
 export default Blog
