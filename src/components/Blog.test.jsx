5eeedcbc63d5235503c63d4f0f78b76eb94b526e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-06-05 16:39:49 +0100

    Add 5.14: Blog List Tests, step 2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,23 +1,32 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { beforeEach } from 'vitest'
 
-test('Blog renders the blog\'s title and author',() => {
+let container
+beforeEach(() => {
   const blog ={
     title : 'test blog',
     author : 'tester',
     url: 'hello.com'
   }
-  render(<Blog blog={blog} />)
+  container = render(<Blog blog={blog} />).container
+})
+test('Blog renders the blog\'s title and author',() => {
   const element = screen.getByText('test blog tester')
   expect(element).toBeDefined()
 })
 test('Blog does not render its URL or number of likes by default',() => {
-  const blog ={
-    title : 'test blog',
-    author : 'tester',
-    url: 'hello.com'
-  }
-  const { container } = render(<Blog blog={blog}/>)
   const div = container.querySelector('.hiddenInfo')
   expect(div).toBeNull()
+})
+test('Blog render URL and number of likes after view button click', async () => {
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+  const div = container.querySelector('.hiddenInfo')
+  expect(div).toBeDefined()
+  expect(div).toHaveTextContent(
+    'hello.comlikes'
+  )
 })
