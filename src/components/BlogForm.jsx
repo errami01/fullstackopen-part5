580c931e3742bbe464663338644b5013742cae49
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-06-07 12:59:25 +0100

    Add 5.16: Blog List Tests, step 4

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,54 +1,57 @@
-import { useState } from "react"
+import { useState } from 'react'
 const BlogForm = ({
-    createBlog,
-})=>{
-  const [newBlog, setNewBlog] = useState({title:'', author:'', url:''})
-  const {title, author, url} = newBlog
+  createBlog,
+}) => {
+  const [newBlog, setNewBlog] = useState({ title:'', author:'', url:'' })
+  const { title, author, url } = newBlog
 
-  const handleSubmit = (event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault()
     createBlog(newBlog, setNewBlog)
   }
-  const handleInputChange = (input, value)=>{
+  const handleInputChange = (input, value) => {
     setNewBlog({
-        ...newBlog,
-        [input]: value
+      ...newBlog,
+      [input]: value
     })
   }
-    return(
-        <div>
+  return(
+    <div>
       <h1>create new</h1>
       <form onSubmit={handleSubmit}>
         <div>
           title
-            <input
+          <input
             type="text"
             value={title}
             name="title"
             onChange={({ target }) => handleInputChange('title', target.value)}
+            placeholder='title'
           />
         </div>
         <div>
           author
-            <input
+          <input
             type="text"
             value={author}
             name="Author"
             onChange={({ target }) => handleInputChange('author', target.value)}
+            placeholder='author'
           />
         </div>
         <div>
           url
-            <input
+          <input
             type="text"
             value={url}
             name="Url"
             onChange={({ target }) => handleInputChange('url', target.value)}
+            placeholder='url'
           />
         </div>
         <button type="submit">create</button>
       </form>
     </div>
-    )
+  )
 }
 export default BlogForm
