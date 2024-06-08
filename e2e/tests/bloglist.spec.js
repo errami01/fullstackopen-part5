2aed44d62bb10528fe834a4e0270b75d83a32bd8
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-06-08 18:15:42 +0100

    Add 5.18: Blog List End To End Testing, step 2

--- e2e/tests/bloglist.spec.js
+++ e2e/tests/bloglist.spec.js
@@ -1,11 +1,34 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
-      await page.goto('http://localhost:5173')
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'nki',
+                username: 'abdo',
+                password: 'salainen'
+            }
+    })
+        await page.goto('http://localhost:5173')
     })
   
     test('Login form is shown', async ({ page }) => {
         await expect(page.getByTestId('login-form')).toBeVisible();
     })
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('abdo')
+            await page.getByTestId('password').fill('salainen') 
+            await page.getByRole('button', { name: 'login' }).click() 
+            await expect(page.getByText('nki abdo is logged in')).toBeVisible()
+        })
+    
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('abdo')
+            await page.getByTestId('password').fill('salain') 
+            await page.getByRole('button', { name: 'login' }).click() 
+            await expect(page.getByText('Wrong username or passowrd')).toBeVisible()
+        })
+      })
 })
